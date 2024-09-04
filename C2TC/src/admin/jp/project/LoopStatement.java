efff90ba7e3e2667bb38d66e44f8e49509c07935
praka <praka@JAYAPRAKASH>
2024-09-04 14:05:39 +0530

    loop

---
+++ C2TC/src/admin/jp/project/LoopStatement.java
@@ -0,0 +1,35 @@
+package admin.jp.project;
+
+public class LoopStatement {
+
+	    public static void main(String[] args) {
+	        // 1. for loop
+	        System.out.println("For Loop:");
+	        for (int i = 1; i <= 5; i++) {
+	            System.out.println("i = " + i);
+	        }
+	        System.out.println();
+
+	        // 2. while loop
+	        System.out.println("While Loop:");
+	        int j = 1;
+	        while (j <= 5) {
+	            System.out.println("j = " + j);
+	            j++;
+	        }
+	        System.out.println();
+
+	        // 3. do-while loop
+	        System.out.println("Do-While Loop:");
+	        int k = 1;
+	        do {
+	            System.out.println("k = " + k);
+	            k++;
+	        } while (k <= 5);
+	        System.out.println();
+
+	       
+	        }
+	    
+	
+}
